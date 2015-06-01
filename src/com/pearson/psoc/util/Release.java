9d274ad3216fda3bf90e4672d96350fcb3c1cab8
Mohammed Saquib <git.saquib@gmail.com>
2015-06-01 17:53:59 +0530

    refactor

---
+++ src/com/pearson/psoc/util/Release.java
@@ -0,0 +1,35 @@
+package com.pearson.psoc.util;
+
+/**
+ * 
+ * @author Mohammed Saquib (mohammed.saquib)
+ *
+ */
+public class Release {
+	
+	private String releaseName;
+	private String releaseStartDate;
+	private String releaseEndDate;
+	
+	public String getReleaseName() {
+		return releaseName;
+	}
+	public void setReleaseName(String releaseName) {
+		this.releaseName = releaseName;
+	}
+	public String getReleaseStartDate() {
+		return releaseStartDate;
+	}
+	public void setReleaseStartDate(String releaseStartDate) {
+		this.releaseStartDate = releaseStartDate;
+	}
+	public String getReleaseEndDate() {
+		return releaseEndDate;
+	}
+	public void setReleaseEndDate(String releaseEndDate) {
+		this.releaseEndDate = releaseEndDate;
+	}
+	
+	
+
+}
